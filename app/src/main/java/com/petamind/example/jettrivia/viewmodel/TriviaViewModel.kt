25623debee92e907ee5283d33b740903da0da43c
tung <tungsd@gmail.com>
2022-04-06 23:25:07 +1200

    Optimise imports

--- app/src/main/java/com/petamind/example/jettrivia/viewmodel/TriviaViewModel.kt
+++ app/src/main/java/com/petamind/example/jettrivia/viewmodel/TriviaViewModel.kt
@@ -1,16 +1,12 @@
 package com.petamind.example.jettrivia.viewmodel
 
-import android.util.Log
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.petamind.example.jettrivia.model.TriviaItem
 import com.petamind.example.jettrivia.repository.TriviaRepository
-import com.petamind.example.jettrivia.util.LOG_TAG
 import com.squareup.moshi.Types
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import javax.inject.Inject
